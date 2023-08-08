604d1de104489b79cbd40092f5a308a8738919e8
RomiGTapia <romigodoytapia@gmail.com>
2023-08-08 15:34:10 -0300

    Update EjemploParaGit.java
    Te mande un saludo

--- src/ejemploparagit/EjemploParaGit.java
+++ src/ejemploparagit/EjemploParaGit.java
@@ -15,7 +15,7 @@ public class EjemploParaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Rodrigo Gabriel");
     }
 
 }
